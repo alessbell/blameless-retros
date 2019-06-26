90567264ce5e460981a3eadb8b5135d4fac2dfc9
Alessia Bellisario <alessia@breather.com>
2019-06-26 14:51:13 -0400

    First commit

---
+++ theme.js
@@ -0,0 +1,15 @@
+import { swiss as theme } from 'mdx-deck/themes';
+// import nightOwl from 'prism-react-renderer/themes/nightOwl';
+
+export default {
+  ...theme,
+  li: {
+    margin: '1rem',
+  },
+  // table: {
+  //   padding: '2rem',
+  // },
+  Slide: {
+    padding: '2rem',
+  },
+};
